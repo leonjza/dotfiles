3598a65c7216589642647bd573981f6c2dcf594c
Leon Jacobs <leonja511@gmail.com>
2016-09-13 09:26:20 +0200

    Add alias for gdb and add some checks for commands

--- dotfiles.d/zshrc.d/aliases.zsh
+++ dotfiles.d/zshrc.d/aliases.zsh
@@ -4,14 +4,12 @@
 # Checks can be done to see if a command exists before building
 # the alias.
 
+# Simple aliases
+
 # Access to this dotfile installer
 alias dotfiles="bash <(curl -fsSL https://raw.githubusercontent.com/leonjza/dotfiles/master/install)"
-
-# Simple aliases
-alias chain="proxychains4 -q"
+# Please do the last command as sudo
 alias please='sudo $(fc -ln -1)'
-alias p="passpie"
-alias nah="git reset --hard;git clean -df"
 
 # Alias Screensaver on osx as afk. My Mac locks when it starts.
 if [[ -f "/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine" ]]; then
@@ -31,4 +29,31 @@ fi
 # Alias locate on macOS to Spotlights mdfind -name
 if hash mdfind 2>/dev/null; then
     alias locate="mdfind -name"
+fi
+
+# Make gdb quiet on start
+if hash gdb 2>/dev/null; then
+    alias gdb="gdb -q"
+fi
+
+# Alias nah to reset changes in a git repository
+if hash git 2>/dev/null; then
+    alias nah="git reset --hard;git clean -df"
+fi
+
+# Alias proxychains. macOS & brew seems to have it as proxychanins4
+# and *everywhere* else its just proxychains
+if hash proxychains 2>/dev/null || hash proxychains4 2>/dev/null; then
+
+    if hash proxychains 2>/dev/null; then
+        alias chain="proxychains -q"
+    else
+        alias chain="proxychains4 -q"
+    fi
+
+fi
+
+# Alias passpie password manager. Install with: brew install passpie
+if hash passpipe 2>/dev/null; then
+    alias p="passpie"
 fi
