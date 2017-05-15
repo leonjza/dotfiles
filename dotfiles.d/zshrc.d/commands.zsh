d6a8cbb0a5d3a53d4b6ff34d46b9dd0d7fc9190c
Leon Jacobs <leonja511@gmail.com>
2017-05-15 08:44:21 +0200

    Replace archey with neofetch.

--- dotfiles.d/zshrc.d/commands.zsh
+++
@@ -1,8 +0,0 @@
-# Commands
-#
-# Commands specified here run on shell startup
-
-# Run Archey if available! Install with: brew install archey
-if hash archey 2>/dev/null; then
-    archey -c -o
-fi
---
+++ dotfiles.d/zshrc.d/startup.zsh
@@ -0,0 +1,8 @@
+# Commands
+#
+# Commands specified here run on shell startup
+
+# Run neofetch if available.
+if hash neofetch 2>/dev/null; then
+    neofetch
+fi
