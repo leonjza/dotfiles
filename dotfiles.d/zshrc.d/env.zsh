18cf792ecf54d2211dba3ae15d87449024554bfb
Leon Jacobs <leonja511@gmail.com>
2019-07-19 08:42:12 +0200

    Add fzf ctrl+t preview configuration.

--- dotfiles.d/zshrc.d/env.zsh
+++ dotfiles.d/zshrc.d/env.zsh
@@ -7,3 +7,16 @@ export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
 
 # Composer, globally installed binaries
 export PATH="$PATH:$HOME/.composer/vendor/bin"
+
+# https://github.com/silvanocerza/dotfiles/blob/master/zsh/zshrc#L46
+#
+# If current selection is a text file shows its content,
+# if it's a directory shows its content, the rest is ignored
+FZF_CTRL_T_OPTS="--preview-window wrap --preview '
+if [[ -f {} ]]; then
+    file --mime {} | grep -q \"text\/.*;\" && bat --color \"always\" {} || (tput setaf 1; file --mime {})
+elif [[ -d {} ]]; then
+    exa -l --color always {}
+else;
+    tput setaf 1; echo YOU ARE NOT SUPPOSED TO SEE THIS!
+fi'"
